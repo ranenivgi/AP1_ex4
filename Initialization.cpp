6600fa70f820bd132f1c4266a1f9b8323c68aa32
yahel frankel <yahel.frankel@gmail.com>
2022-12-14 00:00:32 +0200

    delete an unused function

--- Initialization.cpp
+++ Initialization.cpp
@@ -5,7 +5,7 @@ vector<pair<string, vector<double>>> readFromFile(string filePath) {
     stringstream ss;
 
     // Open the file for reading.
-    ifstream file("datasets/iris/iris_classified.csv");
+    ifstream file(filePath);
 
     // Check if the file was opened successfully.
     if (!file.is_open()) {
@@ -43,31 +43,31 @@ vector<pair<string, vector<double>>> readFromFile(string filePath) {
     return database;
 }
 
-/**
- * @brief this function converts string input to vector using stringstream object.
- *
- * @param str the string
- * @return vector<double> the vector
- */
-vector<double> strToVec(string str)
-{
-    vector<double> vec;
-    double num;
-    stringstream ss;
+// /**
+//  * @brief this function converts string input to vector using stringstream object.
+//  *
+//  * @param str the string
+//  * @return vector<double> the vector
+//  */
+// vector<double> strToVec(string str)
+// {
+//     vector<double> vec;
+//     double num;
+//     stringstream ss;
 
-    // insert the string to the ss
-    ss << str;
+//     // insert the string to the ss
+//     ss << str;
 
-    // insert the numbers to the vector (split by spaces)
-    while (!ss.eof())
-    {
-        // checking if the input is valid
-        if (!(ss >> num))
-        {
-            cout << "invalid input" << endl;
-            exit(1);
-        }
-        vec.push_back(num);
-    }
-    return vec;
-}
+//     // insert the numbers to the vector (split by spaces)
+//     while (!ss.eof())
+//     {
+//         // checking if the input is valid
+//         if (!(ss >> num))
+//         {
+//             cout << "invalid input" << endl;
+//             exit(1);
+//         }
+//         vec.push_back(num);
+//     }
+//     return vec;
+// }
